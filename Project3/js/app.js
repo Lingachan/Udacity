9432b0f583b0311808b29953f676a45cc95b5eac
linga <lingachandar.pc2013@vit.ac.in>
2018-08-22 05:39:30 +0530

    update

--- Project3/js/app.js
+++ Project3/js/app.js
@@ -1,14 +1,40 @@
-// Declare card symbols
-let icons = ["fa-diamond", "fa-diamond", "fa-paper-plane-o", "fa-paper-plane-o", "fa-anchor", "fa-anchor", "fa-bolt", "fa-bolt", "fa-cube", "fa-cube", "fa-leaf", "fa-leaf", "fa-bicycle", "fa-bicycle", "fa-bomb", "fa-bomb"];
+// timer
+function timeFormat ( val ) {
+  return val > 9 ? val : "0" + val; 
+ }
 
-// Create array to hold opened cards
+function timer() {
+ let timer;
+ let clicks = 0;
+ $(".card").on("click", function() {
+   clicks += 1;
+   if (clicks === 1) {
+     var sec = 0;
+     timer = setInterval( function(){
+       $("#sec").html(timeFormat(++sec % 60));
+       $("#min").html(timeFormat(parseInt(sec / 60, 10)));
+     }, 1000);
+   }
+ })
+}
+
+// Function to restart the game on icon click
+function restartGame() {
+  $("#restart").on("click", function() {
+      location.reload()
+  });
+  }
+
+// card symbols
+let cards = ["fa-diamond", "fa-diamond", "fa-paper-plane-o", "fa-paper-plane-o", "fa-anchor", "fa-anchor", "fa-bolt", "fa-bolt", "fa-cube", "fa-cube", "fa-leaf", "fa-leaf", "fa-bicycle", "fa-bicycle", "fa-bomb", "fa-bomb"];
+
+// global variables
 let openCard = [];
 let moves = 0;
 let starts = 3;
-let matchFound = 0;
+let similarCard = 0;
 let startGame = false;
-let star = "3";
-let timer;
+let starRating = "3";
 
 // Shuffle cards (function from http://stackoverflow.com/a/2450976)
 function shuffle(array) {
@@ -24,43 +50,56 @@ function shuffle(array) {
     return array;
 }
 
-// Create each card's HTMl
+// Create card
 function createCard() {
-  let cardList = shuffle(icons);
-  cardList.forEach(function(card) {
+  let cardType = shuffle(cards);
+  cardType.forEach(function(card) {
     $(".deck").append('<li><i class="card fa ' + card + '"></i></li>');
   })
 }
 
-// find matching cards
-function findMatch() {
-  // Show cards on click
+// Update stars
+function updateStars() {
+  if (moves === 1) {
+    $("#moveTxt").text(" Move");
+  } else {
+    $("#moveTxt").text(" Moves");
+  }
+  $("#moves").text(moves.toString());
+
+  if (moves > 0 && moves < 16) {
+    starRating = starRating;
+  } else if (moves >= 16 && moves <= 24) {
+    $("#one").removeClass("fa-star");
+    starRating = "2";
+  } else if (moves > 24) {
+    $("#two").removeClass("fa-star");
+    starRating = "1";
+  }
+}
+
+// Mathching card
+function cardMatch() {
   $(".card").on("click", function() {
     if ($(this).hasClass("open show")) { return; }
-    $(this).toggleClass("flipIn open show");
+    $(this).toggleClass("flipIt open show");
     openCard.push($(this));
     startGame = true;
-   // Check if classlist matches when openCard length == 2
     if (openCard.length === 2) {
       if (openCard[0][0].classList[2] === openCard[1][0].classList[2]) {
-      openCard[0][0].classList.add("bounceIn", "match");
-      openCard[1][0].classList.add("bounceIn", "match");
+      openCard[0][0].classList.add("pop", "match");
+      openCard[1][0].classList.add("pop", "match");
       $(openCard[0]).off('click');
       $(openCard[1]).off('click');
-      matchFound += 1;
+      similarCard += 1;
       moves++;
-      removeOpenCards();
-      if(matchFound === 8) {
-      findWinner();
-    }
+      closeCards();
+      gameResult();
       } else {
-      // If classes don't match, add "wrong" class
       openCard[0][0].classList.add("shake", "wrong");
       openCard[1][0].classList.add("shake", "wrong");
-      // Set timeout to remove "show" and "open" class
-      setTimeout(removeClasses, 1100);
-      // Reset openCard.length to 0
-      setTimeout(removeOpenCards, 1100);
+      setTimeout(removeClasses, 500);
+      setTimeout(closeCards, 500);
       moves++;
       }
     }
@@ -68,74 +107,15 @@ function findMatch() {
   })
 }
 
-// Update stars
-function updateStars() {
-  if (moves === 1) {
-    $("#movesText").text(" Move");
-  } else {
-    $("#movesText").text(" Moves");
-  }
-  $("#moves").text(moves.toString());
-
-  if (moves > 0 && moves < 16) {
-    star = star;
-  } else if (moves >= 16 && moves <= 20) {
-    $("#starOne").removeClass("fa-star");
-    star = "2";
-  } else if (moves > 20) {
-    $("#starTwo").removeClass("fa-star");
-    star = "1";
-  }
-}
-
-// Start timer on the first card click
-function startTimer() {
-  let clicks = 0;
-  $(".card").on("click", function() {
-    clicks += 1;
-    if (clicks === 1) {
-      var sec = 0;
-      function time ( val ) { return val > 9 ? val : "0" + val; }
-      timer = setInterval( function(){
-        $(".seconds").html(time(++sec % 60));
-        $(".minutes").html(time(parseInt(sec / 60, 10)));
-      }, 1000);
-    }
-  })
- }
-
-// Open popup when game is complete
-function findWinner() {
-  if (matchFound === 8) {
-    var modal = document.getElementById('win-popup');
-    var span = document.getElementsByClassName("close")[0];
-
-    $("#total-moves").text(moves);
-    $("#total-stars").text(star);
-
-    modal.style.display = "block";
-
-  // When the user clicks on <span> (x), close the modal
-    span.onclick = function() {
-        modal.style.display = "none";
-    }
-
-   $("#play-again-btn").on("click", function() {
-       location.reload()
-   });
-
-   clearInterval(timer);
-}
-}
 // Reset openCard.length to 0
-function removeOpenCards() {
+function closeCards() {
   openCard = [];
 }
 
-// Remove all classes except "match"
+// Remove all classes
 function removeClasses() {
-  $(".card").removeClass("show open flipIn bounceIn shake wrong");
-  removeOpenCards();
+  $(".card").removeClass("show open flipIt pop shake wrong");
+  closeCards();
 }
 
 // Disable clicks
@@ -145,17 +125,31 @@ function disableClick() {
   })
 }
 
-// Call functions
-shuffle(icons);
-createCard();
-findMatch();
-startTimer();
+// Open popup
+function gameResult() {
 
-// Function to restart the game on icon click
-function restartGame() {
-  $("#restart").on("click", function() {
-      location.reload()
+  if (similarCard === 8) {
+    swal({
+      title: 'Congratulations',
+      type: 'success',
+      text: 'You have won the game with ' + moves + '. You got ' + starRating + ' Stars.',
+      allowOutsideClick: false,
+      showCancelButton: true,
+      confirmButtonText: 'Play Again',
+      confirmButtonColor: '#02ccba',
+      cancelButtonText: 'Close',
+      cancelButtonColor: '#aa7ecd'
+  }).then(function() {
+      location.reload();
+  }, function(dismiss) {
+      console.log('Yes');
   });
-  }
+ }
+}
 
+// Call functions
+shuffle(cards);
+createCard();
+cardMatch();
+timer();
 restartGame();
